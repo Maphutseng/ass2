ffa640129f59d66730771294563bf1830d444be5
Maphutseng <mphetholemaphutseng@gmail.com>
2025-04-06 12:02:23 -0700

    Delete spane/src/main directory

--- spane/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.spane {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.example.spane to javafx.fxml;
-    exports com.example.spane;
-}
